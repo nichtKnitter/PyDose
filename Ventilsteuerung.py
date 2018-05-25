d3f307229910c8ad8dc173fad970b1ba59c66e39
Max Baumgartner <baumax@posteo.de>
2018-05-25 17:40:36 +0200

    state dictionary eingeführt, zugriff ist aber momentan kaputt, kriege für alles immer "V1" zurück, auch wenn ich versuche andere anzuwählen

--- Ventilsteuerung.py
+++ Ventilsteuerung.py
@@ -8,6 +8,18 @@ from nidaqmx.constants import (LineGrouping)
 pp = pprint.PrettyPrinter(indent=4)
 import time
 
+# State dictionary, um nur notwendige Venitle zu schalten
+v_state = [
+    {"id": "V1", "state": "NA", "active": "NA"},
+    {"id": "V2", "state": "NA", "active": "NA"},
+    {"id": "V3", "state": "NA", "active": "NA"},
+    {"id": "V4", "state": "NA", "active": "NA"},
+    {"id": "V5", "state": "NA", "active": "NA"},
+    {"id": "V6", "state": "NA", "active": "NA"},
+    {"id": "V7", "state": "NA", "active": "NA"},
+]
+
+
 
 def Ventiladressen(Ventilname):
     if (Ventilname == "V1"):
@@ -35,31 +47,41 @@ def Ventiladressen(Ventilname):
     return (Adress)
 
 
-def Ventil_schalten(Ventilname, Befehl):
+def Ventil_schalten(Ventilname, Befehl_in, v_state):
+    print("ventilname in Ventil schalten", Ventilname)
     Ventil_auf = [True, False]
     Ventil_zu = [False, True]
     Ventil_aus = [True, True]
     Ventiladresse = Ventiladressen(Ventilname)
-    if (Befehl == "auf"):
+    if (Befehl_in == "auf"):
         Befehl = Ventil_auf
-    if (Befehl == "zu"):
+    if (Befehl_in == "zu"):
         Befehl = Ventil_zu
-    if (Befehl == "aus"):
+    if (Befehl_in == "aus"):
         Befehl = Ventil_aus
     print("Befehl", Befehl)
     with nidaqmx.Task() as VentilTask:
         print(Ventilname, Befehl)
-        VentilTask.do_channels.add_do_chan(Ventiladresse,
-                                           line_grouping=LineGrouping.CHAN_PER_LINE)
+        VentilTask.do_channels.add_do_chan(Ventiladresse, line_grouping=LineGrouping.CHAN_PER_LINE)
         try:
-            print(VentilTask.write(Befehl))  # beide kanäle an, wird nie gebaucht
+            (VentilTask.write(Befehl))  # beide kanäle an, wird nie gebaucht
+            v_state["id" == Ventilname]["active"] = True
+            v_state["id" == Ventilname]["state"] = Befehl_in
+            print("in Ventil.task", v_state["id" == Ventilname])
         except nidaqmx.DaqError as e:
             print(e)
+
         time.sleep(0.5)
+
         try:
-            print(VentilTask.write(Ventil_aus))  # beide kanäle an, wird nie gebaucht
+            (VentilTask.write(Ventil_aus))  # beide kanäle an, wird nie gebaucht
+            v_state["id" == Ventilname]["active"] = False
+            v_state["id" == Ventilname]["state"] = "aus"
+            print("in Ventil.task", v_state["id" == Ventilname])
         except nidaqmx.DaqError as e:
             print(e)
+    print("in ventil schlaten", v_state)
+    return (v_state)
 
 
 def alle_aus():
@@ -72,14 +94,22 @@ def alle_aus():
     Ventil_schalten("V7", "aus")
 
 
-def alle_auf():
-    Ventil_schalten("V1", "auf")
-    Ventil_schalten("V2", "auf")
-    Ventil_schalten("V3", "auf")
-    Ventil_schalten("V4", "auf")
-    Ventil_schalten("V5", "auf")
-    Ventil_schalten("V6", "auf")
-    Ventil_schalten("V7", "auf")
+def alle_auf(v_state):
+    v_state = Ventil_schalten("V1", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V2", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V3", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V4", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V5", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V6", "auf", v_state)
+    print("in alle_auf", v_state)
+    v_state = Ventil_schalten("V7", "auf", v_state)
+    print("in alle_auf", v_state)
+    return (v_state)
 
 
 def Volumen_evak_grob():
@@ -93,4 +123,5 @@ def Volumen_evak_grob():
 
 
 # Volumen_evak_grob()
-alle_auf()
+v_state = alle_auf(v_state)
+print(v_state)
